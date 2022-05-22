6d3368d6c9094fcbc683dd8f7fd39d89c60a8bc5
Okellothomas <thomasolal33@gmail.com>
2022-05-22 12:57:25 +0300

    add my comment

---
+++ app/src/main/java/com/pro/myrestaurant/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pro.myrestaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*
+    This is what we have been doing
+     */
+}
